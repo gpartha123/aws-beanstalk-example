b2742a6f3ab133ab9cf3e40bfa64037bbc9a5dac
gpartha123 <gpartha.123@gmail.com>
2020-01-19 16:34:59 +0530

    Firt Commit

---
+++ src/main/java/com/pat/aws/AwsElasticbeanstalkExample2Application.java
@@ -0,0 +1,13 @@
+package com.pat.aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsElasticbeanstalkExample2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsElasticbeanstalkExample2Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/pat/aws/HelloController.java
@@ -0,0 +1,16 @@
+package com.pat.aws;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/hello")
+@RestController
+public class HelloController {
+	
+	public String hello()
+	{
+		return "Hello Welcome";
+	}
+	
+
+}
